dcc54091a7177a990a55f9340a8bdf43025aabdc
zLuCaS2K <JoseLucas50000@Gmail.com>
2020-02-08 07:54:40 -0300

    Inicio da Implementação do RecyclerView

---
+++ app/src/main/java/com/lucasprojects/mytask/adapter/TaskListAdapter.kt
@@ -0,0 +1,30 @@
+package com.lucasprojects.mytask.adapter
+/*
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lucasprojects.mytask.entities.TaskEntity
+import com.lucasprojects.mytask.entities.listenner.OnTaskListFragmentInteractionListenner
+import com.lucasprojects.mytask.holder.TaskViewHolder
+
+class TaskListAdapter(
+    tasklist: List<TaskEntity>,
+    listener: OnTaskListFragmentInteractionListenner
+) : RecyclerView.Adapter<TaskViewHolder>() {
+
+    private val mListTaskEntity: List<TaskEntity> = tasklist
+    private val listener: OnTaskListFragmentInteractionListenner = listener
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+
+    }
+
+    /** Metodo que conta o tamanho da lista de tasks */
+    override fun getItemCount(): Int {
+        return mListTaskEntity.size
+
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+
+    }
+}*/
---
+++ app/src/main/java/com/lucasprojects/mytask/holder/TaskViewHolder.kt
@@ -0,0 +1,9 @@
+package com.lucasprojects.mytask.holder
+
+import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskViewHolder(itemView: View, val context: Context) : RecyclerView.ViewHolder(itemView) {
+
+}
