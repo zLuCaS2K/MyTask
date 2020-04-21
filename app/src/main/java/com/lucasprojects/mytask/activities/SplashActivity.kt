acf1e848726a934ffdb3eea24ba5d17521700a87
zLuCaS2K <JoseLucas50000@Gmail.com>
2020-04-21 08:15:04 -0300

    Animações na Splash e RecyclerView

--- app/src/main/java/com/lucasprojects/mytask/activities/SplashActivity.kt
+++ app/src/main/java/com/lucasprojects/mytask/activities/SplashActivity.kt
@@ -3,8 +3,10 @@ package com.lucasprojects.mytask.activities
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.lucasprojects.mytask.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
@@ -12,9 +14,12 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val animation = AnimationUtils.loadAnimation(this, R.anim.bounce)
+        appName.startAnimation(animation)
+
         Handler().postDelayed({
             startActivity(Intent(this, SlideActivity::class.java))
             finish()
-        }, 1000)
+        }, 1200)
     }
 }
