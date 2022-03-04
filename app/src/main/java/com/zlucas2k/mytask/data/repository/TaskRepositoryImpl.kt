2c55fe826c6846d5b17864cbe68a8ce324c3a420
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-03-04 14:50:05 -0300

    Implementação completa do TaskRepositoryImpl

--- app/src/main/java/com/zlucas2k/mytask/data/repository/TaskRepositoryImpl.kt
+++ app/src/main/java/com/zlucas2k/mytask/data/repository/TaskRepositoryImpl.kt
@@ -1,23 +1,31 @@
 package com.zlucas2k.mytask.data.repository
 
+import com.zlucas2k.mytask.common.mapper.TaskMapperImpl
 import com.zlucas2k.mytask.data.database.TaskDAO
 import com.zlucas2k.mytask.domain.model.Task
 import com.zlucas2k.mytask.domain.repository.TaskRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class TaskRepositoryImpl(
     private val taskDAO: TaskDAO
 ) : TaskRepository {
 
     override fun getAllTask(): Flow<List<Task>> {
-        TODO("Implementar Listagem")
+        return taskDAO.getAllTask().map { listTaskEntity ->
+            listTaskEntity.map { taskEntity ->
+                TaskMapperImpl.mapEntityToModel(taskEntity)
+            }
+        }
     }
 
     override suspend fun saveTask(task: Task) {
-        TODO("Implementar Inserção")
+        val taskEntity = TaskMapperImpl.mapModelToEntity(task)
+        taskDAO.saveTask(taskEntity)
     }
 
     override suspend fun deleteTask(task: Task) {
-        TODO("Implementar Exclução")
+        val taskEntity = TaskMapperImpl.mapModelToEntity(task)
+        taskDAO.saveTask(taskEntity)
     }
 }
