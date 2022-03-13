990ebc5e4b1866fda117ace13a388388c86f8c36
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-03-13 05:50:19 -0300

    Adicionado função de injeção do GetTaskByIdUseCase

--- app/src/main/java/com/zlucas2k/mytask/di/PresentationModule.kt
+++ app/src/main/java/com/zlucas2k/mytask/di/PresentationModule.kt
@@ -3,6 +3,7 @@ package com.zlucas2k.mytask.di
 import com.zlucas2k.mytask.domain.repository.TaskRepository
 import com.zlucas2k.mytask.domain.usecases.DeleteTaskUseCase
 import com.zlucas2k.mytask.domain.usecases.GetAllTaskUseCase
+import com.zlucas2k.mytask.domain.usecases.GetTaskByIdUseCase
 import com.zlucas2k.mytask.domain.usecases.SaveTaskUseCase
 import dagger.Module
 import dagger.Provides
@@ -20,6 +21,12 @@ object PresentationModule {
     @Singleton
     fun provideGetAllTaskUseCase(repository: TaskRepository): GetAllTaskUseCase {
         return GetAllTaskUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetTaskByIdUseCase(repository: TaskRepository): GetTaskByIdUseCase {
+        return GetTaskByIdUseCase(repository)
     }
 
     @Provides
