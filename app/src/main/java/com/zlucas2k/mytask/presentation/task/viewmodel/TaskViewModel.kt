caba2304d26eebfcf4e5f5a8c0311fca1f3d3ef0
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-03-13 07:16:30 -0300

    Melhorias no TaskViewModel

--- app/src/main/java/com/zlucas2k/mytask/presentation/task/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/task/viewmodel/TaskViewModel.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.viewModelScope
 import com.zlucas2k.mytask.domain.model.Priority
 import com.zlucas2k.mytask.domain.model.Status
 import com.zlucas2k.mytask.domain.model.Task
+import com.zlucas2k.mytask.domain.usecases.DeleteTaskUseCase
 import com.zlucas2k.mytask.domain.usecases.GetTaskByIdUseCase
 import com.zlucas2k.mytask.domain.usecases.SaveTaskUseCase
 import com.zlucas2k.mytask.presentation.task.common.TaskState
@@ -19,34 +20,29 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TaskViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
+    private val savedStateHandle: SavedStateHandle,
     private val saveTaskUseCase: SaveTaskUseCase,
-    private val getTaskByIdUseCase: GetTaskByIdUseCase,
+    private val deleteTaskUseCase: DeleteTaskUseCase,
+    private val getTaskByIdUseCase: GetTaskByIdUseCase
 ) : ViewModel() {
 
     private val _state: MutableState<TaskState> = mutableStateOf(TaskState())
     val state: State<TaskState> = _state
-
-    private val _selectId = mutableStateOf(-1)
-    private val _title = mutableStateOf("")
-    private val _date = mutableStateOf("")
-    private val _time = mutableStateOf("")
-    private val _description = mutableStateOf("")
-    private val _priority = mutableStateOf(Priority.NONE)
-    private val _status = mutableStateOf(Status.TODO)
 
     init {
         savedStateHandle.get<Int>("id")?.let { taskId ->
             if (taskId != -1) {
                 viewModelScope.launch {
                     getTaskByIdUseCase(taskId)?.also { task ->
-                        _selectId.value = task.id
-                        _title.value = task.title
-                        _date.value = task.date
-                        _time.value = task.time
-                        _description.value = task.description
-                        _priority.value = task.priority
-                        _status.value = task.status
+                        _state.value = TaskState(
+                            selectedId = task.id,
+                            title = task.title,
+                            date = task.date,
+                            time = task.time,
+                            description = task.description,
+                            priority = task.priority,
+                            status = task.status
+                        )
                     }
                 }
             }
@@ -56,39 +52,54 @@ class TaskViewModel @Inject constructor(
     fun onSaveNote() {
         viewModelScope.launch(Dispatchers.IO) {
             val task = Task(
-                id = _selectId.value,
-                title = _title.value,
-                date = _date.value,
-                time = _time.value,
-                description = _description.value,
-                priority = _priority.value,
-                status = _status.value
+                id = _state.value.selectedId,
+                title = _state.value.title,
+                date = _state.value.date,
+                time = _state.value.time,
+                description = _state.value.description,
+                priority = _state.value.priority,
+                status = _state.value.status
             )
             saveTaskUseCase(task)
         }
     }
 
+    fun onDeleteNote() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val task = Task(
+                id = _state.value.selectedId,
+                title = _state.value.title,
+                date = _state.value.date,
+                time = _state.value.time,
+                description = _state.value.description,
+                priority = _state.value.priority,
+                status = _state.value.status
+            )
+            deleteTaskUseCase(task)
+        }
+    }
+
     fun onTitleChange(newText: String) {
-        _title.value = newText
+        _state.value = _state.value.copy(title = newText)
     }
 
     fun onDateChange(newText: String) {
-        _date.value = newText
+        _state.value = _state.value.copy(date = newText)
     }
 
     fun onTimeChange(newText: String) {
-        _time.value = newText
+        _state.value = _state.value.copy(time = newText)
     }
 
     fun onDescriptionChange(newText: String) {
-        _description.value = newText
+        _state.value = _state.value.copy(description = newText)
     }
 
     fun onPriorityChange(newPriority: Priority) {
-        _priority.value = newPriority
+        _state.value = _state.value.copy(priority = newPriority)
     }
 
     fun onStatusChange(newStatus: Status) {
-        _status.value = newStatus
+        _state.value = _state.value.copy(status = newStatus)
     }
 }
