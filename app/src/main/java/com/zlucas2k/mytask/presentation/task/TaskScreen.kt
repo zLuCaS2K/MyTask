9192eb02b17bf80e6f4856d5d0c1e6fefbb00392
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-03-13 07:17:56 -0300

    Agora a edição ou salvamento vai ser possível pela TopBar da TaskScreen

--- app/src/main/java/com/zlucas2k/mytask/presentation/task/TaskScreen.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/task/TaskScreen.kt
@@ -1,19 +1,59 @@
 package com.zlucas2k.mytask.presentation.task
 
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.zlucas2k.mytask.presentation.task.viewmodel.TaskViewModel
 
 @Composable
-fun TaskScreen(navHostController: NavHostController, viewModel: TaskViewModel = viewModel()) {
+fun TaskScreen(navHostController: NavHostController, viewModel: TaskViewModel = hiltViewModel()) {
 
     val state = viewModel.state.value
+    val isEditing = state.selectedId == -1
 
     Scaffold(
         topBar = {
+            TopAppBar(
+                title = {
 
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navHostController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onPrimary
+                        )
+                    }
+                },
+                actions = {
+                    if (isEditing) {
+                        IconButton(onClick = { viewModel.onDeleteNote() }) {
+                            Icon(
+                                imageVector = Icons.Filled.Delete,
+                                contentDescription = null,
+                                tint = MaterialTheme.colors.onPrimary
+                            )
+                        }
+                    }
+                    IconButton(onClick = { viewModel.onSaveNote() }) {
+                        Icon(
+                            imageVector = Icons.Filled.Done,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onPrimary
+                        )
+                    }
+                },
+                elevation = 0.dp
+            )
         },
         content = {
             TaskScreenContent(
