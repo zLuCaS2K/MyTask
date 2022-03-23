a8a8854d5bb0228fb4c3df30cf6eef40019ad934
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-03-23 14:40:25 -0300

    Injeção da depêndencia do TaskWorkerProvider nos casos de uso

--- app/src/main/java/com/zlucas2k/mytask/di/PresentationModule.kt
+++ app/src/main/java/com/zlucas2k/mytask/di/PresentationModule.kt
@@ -5,6 +5,7 @@ import com.zlucas2k.mytask.domain.usecases.DeleteTaskUseCase
 import com.zlucas2k.mytask.domain.usecases.GetAllTaskUseCase
 import com.zlucas2k.mytask.domain.usecases.GetTaskByIdUseCase
 import com.zlucas2k.mytask.domain.usecases.SaveTaskUseCase
+import com.zlucas2k.mytask.infrastructure.worker.provider.TaskWorkerProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -31,13 +32,13 @@ object PresentationModule {
 
     @Provides
     @Singleton
-    fun provideSaveTaskUseCase(repository: TaskRepository): SaveTaskUseCase {
-        return SaveTaskUseCase(repository)
+    fun provideSaveTaskUseCase(repository: TaskRepository, taskWorkerProvider: TaskWorkerProvider): SaveTaskUseCase {
+        return SaveTaskUseCase(repository, taskWorkerProvider)
     }
 
     @Provides
     @Singleton
-    fun provideDeleteTaskUseCase(repository: TaskRepository): DeleteTaskUseCase {
-        return DeleteTaskUseCase(repository)
+    fun provideDeleteTaskUseCase(repository: TaskRepository, taskWorkerProvider: TaskWorkerProvider): DeleteTaskUseCase {
+        return DeleteTaskUseCase(repository, taskWorkerProvider)
     }
 }
