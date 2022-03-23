7232caa2a643694078120135cddc37aa6ebc1484
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-03-23 14:39:35 -0300

    Implementação do agendamento de tarefa ao deletar

--- app/src/main/java/com/zlucas2k/mytask/domain/usecases/DeleteTaskUseCase.kt
+++ app/src/main/java/com/zlucas2k/mytask/domain/usecases/DeleteTaskUseCase.kt
@@ -2,13 +2,16 @@ package com.zlucas2k.mytask.domain.usecases
 
 import com.zlucas2k.mytask.domain.model.Task
 import com.zlucas2k.mytask.domain.repository.TaskRepository
+import com.zlucas2k.mytask.infrastructure.worker.provider.TaskWorkerProvider
 import javax.inject.Inject
 
 class DeleteTaskUseCase @Inject constructor(
-    private val repository: TaskRepository
+    private val repository: TaskRepository,
+    private val workerProvider: TaskWorkerProvider
 ) {
 
     suspend operator fun invoke(task: Task) {
         repository.deleteTask(task)
+        workerProvider.cancelTaskWork(task)
     }
 }
