4280eccbaf8f2d296b599ace6e8d6d3c579b0d0d
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-03-24 11:41:16 -0300

    Implementação da Splash Screen

--- app/src/main/java/com/zlucas2k/mytask/presentation/MainActivity.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.zlucas2k.mytask.presentation.common.navigation.NavigationComponent
 import com.zlucas2k.mytask.presentation.common.theme.MyTaskTheme
@@ -17,6 +18,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MyTaskTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
