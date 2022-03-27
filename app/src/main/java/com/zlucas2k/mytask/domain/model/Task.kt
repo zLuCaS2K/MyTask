d87c79c8626ae49e1134cac90abcc55816e3d320
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-03-27 15:32:08 -0300

    Adicionado validação de data e hora para as tarefas

--- app/src/main/java/com/zlucas2k/mytask/domain/model/Task.kt
+++ app/src/main/java/com/zlucas2k/mytask/domain/model/Task.kt
@@ -1,5 +1,8 @@
 package com.zlucas2k.mytask.domain.model
 
+import java.util.Calendar
+import com.zlucas2k.mytask.common.utils.Utils
+
 data class Task(
     val id: Int = 0,
     val title: String,
@@ -8,4 +11,15 @@ data class Task(
     val description: String,
     val priority: Priority,
     val status: Status = Status.TODO
-)
+) {
+    fun isNotValidDateAndTimeTask(): Boolean {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+        }
+        val dueTime = Utils.convertStringToCalendar(date, time)
+
+        val diff = dueTime.timeInMillis - calendar.timeInMillis
+
+        return diff < 0
+    }
+}
