e53144c40296a6732ce7b49b690da68add65ae52
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-04-15 07:36:35 -0300

    Melhoria na usabilidade da barra de pesquisa, agora será focada ao ser aberta

--- app/src/main/java/com/zlucas2k/mytask/presentation/home/components/HomeTopAppBar.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/home/components/HomeTopAppBar.kt
@@ -10,8 +10,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -54,6 +58,12 @@ private fun HomeSearchTopAppBar(
     onCloseClicked: () -> Unit,
     onSearchClicked: (String) -> Unit
 ) {
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(key1 = Unit) {
+        focusRequester.requestFocus()
+    }
+
     Surface(
         color = MaterialTheme.colors.primary,
         elevation = AppBarDefaults.TopAppBarElevation,
@@ -115,7 +125,8 @@ private fun HomeSearchTopAppBar(
             colors = TextFieldDefaults.textFieldColors(
                 backgroundColor = Color.Transparent,
                 cursorColor = Color.White.copy(alpha = ContentAlpha.medium)
-            )
+            ),
+            modifier = Modifier.focusRequester(focusRequester)
         )
     }
 }
@@ -161,7 +172,7 @@ private fun HomeSearchTopAppBarPreview() {
             text = "Texto de pesquisa",
             onTextChange = {},
             onCloseClicked = {},
-            onSearchClicked = {}
+            onSearchClicked = {},
         )
     }
 }
