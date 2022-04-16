788a97dbaba3853167e81257321719ceaddf7209
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-04-16 13:16:42 -0300

    Melhoria no design do RoundedButton e adicionado a opção de TextStyle

--- app/src/main/java/com/zlucas2k/mytask/presentation/home/components/buttons/RoundedButton.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/home/components/buttons/RoundedButton.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.zlucas2k.mytask.presentation.common.theme.MyTaskTheme
@@ -20,6 +23,7 @@ import com.zlucas2k.mytask.presentation.common.theme.MyTaskTheme
 fun RoundedButton(
     modifier: Modifier = Modifier,
     text: String,
+    textStyle: TextStyle = MaterialTheme.typography.caption,
     onClick: () -> Unit,
     isSelected: Boolean = true
 ) {
@@ -29,9 +33,16 @@ fun RoundedButton(
         onClick = onClick,
         shape = RoundedCornerShape(percent = 50),
         border = BorderStroke(width = 1.dp, MaterialTheme.colors.onPrimary),
-        modifier = modifier.alpha(selectedAlpha)
+        modifier = modifier.alpha(selectedAlpha),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.Transparent
+        )
     ) {
-        Text(text = text)
+        Text(
+            text = text,
+            style = textStyle,
+            color = MaterialTheme.colors.onPrimary
+        )
     }
 }
 
