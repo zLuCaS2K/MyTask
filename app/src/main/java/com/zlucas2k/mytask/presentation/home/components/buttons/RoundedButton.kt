ab4f5a32c88fbfe4663d6304177c542578888202
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-04-16 08:56:46 -0300

    Implementação do componente RoundedButton

---
+++ app/src/main/java/com/zlucas2k/mytask/presentation/home/components/buttons/RoundedButton.kt
@@ -0,0 +1,60 @@
+package com.zlucas2k.mytask.presentation.home.components.buttons
+
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.zlucas2k.mytask.presentation.common.theme.MyTaskTheme
+
+@Composable
+fun RoundedButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    isSelected: Boolean = true
+) {
+    val selectedAlpha = if (isSelected) 1f else 0.5f
+
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(percent = 50),
+        border = BorderStroke(width = 1.dp, MaterialTheme.colors.onPrimary),
+        modifier = modifier.alpha(selectedAlpha)
+    ) {
+        Text(text = text)
+    }
+}
+
+@Composable
+@Preview(name = "Light", showBackground = true)
+@Preview(name = "Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+private fun PreviewDark() {
+    MyTaskTheme {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            RoundedButton(
+                text = "Hello World",
+                onClick = {},
+                isSelected = true
+            )
+
+            RoundedButton(
+                text = "Hello World",
+                onClick = {},
+                isSelected = false
+            )
+        }
+    }
+}
