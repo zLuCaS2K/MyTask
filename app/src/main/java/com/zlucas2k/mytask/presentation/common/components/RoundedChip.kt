3ddbdb9f1be05f02663fae423c4f0dd83641fb23
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-04-18 09:09:51 -0300

    Mudança do RoundedButton para RoundedChip

--- app/src/main/java/com/zlucas2k/mytask/presentation/common/components/RoundedChip.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/common/components/RoundedChip.kt
@@ -1,46 +1,43 @@
-package com.zlucas2k.mytask.presentation.home.components.buttons
+package com.zlucas2k.mytask.presentation.common.components
 
 import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.zlucas2k.mytask.presentation.common.theme.MyTaskTheme
 
 @Composable
-fun RoundedButton(
+fun RoundedChip(
     modifier: Modifier = Modifier,
     text: String,
-    textStyle: TextStyle = MaterialTheme.typography.caption,
-    onClick: () -> Unit,
-    isSelected: Boolean = true
+    isSelected: Boolean = false,
+    onClick: () -> Unit
 ) {
     val selectedAlpha = if (isSelected) 1f else 0.5f
 
     Button(
         onClick = onClick,
         shape = RoundedCornerShape(percent = 50),
-        border = BorderStroke(width = 1.dp, MaterialTheme.colors.onPrimary),
-        modifier = modifier.alpha(selectedAlpha),
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.Transparent
-        )
+        border = BorderStroke(width = 1.dp, color = MaterialTheme.colors.onPrimary),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+        modifier = modifier.alpha(selectedAlpha)
     ) {
         Text(
             text = text,
-            style = textStyle,
+            style = MaterialTheme.typography.caption,
             color = MaterialTheme.colors.onPrimary
         )
     }
@@ -49,22 +46,29 @@ fun RoundedButton(
 @Composable
 @Preview(name = "Light", showBackground = true)
 @Preview(name = "Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
-private fun PreviewDark() {
+private fun Preview() {
     MyTaskTheme {
         Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxWidth()
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.wrapContentWidth()
         ) {
-            RoundedButton(
-                text = "Hello World",
-                onClick = {},
-                isSelected = true
+            RoundedChip(
+                text = "Todos",
+                isSelected = true,
+                onClick = {
+
+                },
+                modifier = Modifier
             )
 
-            RoundedButton(
-                text = "Hello World",
-                onClick = {},
-                isSelected = false
+            RoundedChip(
+                text = "Todos",
+                isSelected = false,
+                onClick = {
+
+                },
+                modifier = Modifier
             )
         }
     }
