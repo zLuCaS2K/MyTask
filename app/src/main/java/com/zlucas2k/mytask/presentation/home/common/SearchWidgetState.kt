0fed5db2d9c80f4a93373642ac5476f6c597a93c
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-04-19 09:16:21 -0300

    SearchWidgetState movido para o pacote search

--- app/src/main/java/com/zlucas2k/mytask/presentation/home/common/SearchWidgetState.kt
+++
@@ -1,6 +0,0 @@
-package com.zlucas2k.mytask.presentation.home.common
-
-enum class SearchWidgetState {
-    OPENED,
-    CLOSED
-}
---
+++ app/src/main/java/com/zlucas2k/mytask/presentation/home/common/search/SearchWidgetState.kt
@@ -0,0 +1,6 @@
+package com.zlucas2k.mytask.presentation.home.common.search
+
+enum class SearchWidgetState {
+    OPENED,
+    CLOSED
+}
