f2fb2c5809e7bbe4a7ebecdb2249161ed417519b
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-04-19 09:18:06 -0300

    Implementação da lógica de filtragem e melhorias no HomeViewModel

--- app/src/main/java/com/zlucas2k/mytask/presentation/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/home/viewmodel/HomeViewModel.kt
@@ -7,9 +7,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.zlucas2k.mytask.domain.mappers.mapToView
 import com.zlucas2k.mytask.domain.usecases.task.get_all.GetAllTaskUseCase
-import com.zlucas2k.mytask.presentation.common.model.TaskView
-import com.zlucas2k.mytask.presentation.home.common.HomeState
-import com.zlucas2k.mytask.presentation.home.common.SearchWidgetState
+import com.zlucas2k.mytask.presentation.home.common.HomeScreenState
+import com.zlucas2k.mytask.presentation.home.common.filter.TaskStatusFilter
+import com.zlucas2k.mytask.presentation.home.common.filter.TaskStatusFilterWidgetState
+import com.zlucas2k.mytask.presentation.home.common.search.SearchWidgetState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
@@ -20,16 +21,8 @@ class HomeViewModel @Inject constructor(
     private val getAllTaskUseCase: GetAllTaskUseCase
 ) : ViewModel() {
 
-    private val _uiState: MutableState<HomeState> = mutableStateOf(HomeState())
-    val uiState: State<HomeState> get() = _uiState
-
-    private val _searchWidgetState: MutableState<SearchWidgetState> = mutableStateOf(SearchWidgetState.CLOSED)
-    val searchWidgetState: State<SearchWidgetState> get() = _searchWidgetState
-
-    private val _searchTextState: MutableState<String> = mutableStateOf("")
-    val searchTextState: State<String> get() = _searchTextState
-
-    private val _tasksCached: MutableState<List<TaskView>> = mutableStateOf(emptyList())
+    private val _uiState: MutableState<HomeScreenState> = mutableStateOf(HomeScreenState())
+    val uiState: State<HomeScreenState> get() = _uiState
 
     init {
         getAllTasks()
@@ -43,33 +36,69 @@ class HomeViewModel @Inject constructor(
                 }
             }.collect { tasks ->
                 _uiState.value = _uiState.value.copy(tasks = tasks)
-                _tasksCached.value = _uiState.value.tasks
+                _uiState.value = _uiState.value.copy(tasksCached = tasks)
             }
         }
     }
 
-    fun onSearchTask(query: String) {
-        _uiState.value = _uiState.value.copy(isSearching = true)
-        if (query.isEmpty()) {
-            _uiState.value = _uiState.value.copy(tasks = _tasksCached.value)
+    fun onSearchTask() {
+        val searchQuery = _uiState.value.searchQuery
+
+        if (searchQuery.isEmpty()) {
+            val tasksCached = _uiState.value.tasksCached
+            _uiState.value = _uiState.value.copy(tasks = tasksCached)
         } else {
-            val searchResult = _tasksCached.value.filter {
-                it.title.contains(query, true) || it.description.contains(query, true)
+            val searchResult = _uiState.value.tasksCached.filter {
+                it.title.contains(searchQuery, true) || it.description.contains(searchQuery, true)
             }
+
             _uiState.value = _uiState.value.copy(tasks = searchResult)
         }
-        _uiState.value = _uiState.value.copy(isSearching = false)
+    }
+
+    fun onSearchTextChange(newValue: String) {
+        _uiState.value = _uiState.value.copy(searchQuery = newValue)
+    }
+
+    fun onSearchWidgetStateChange() {
+        val newState = when (_uiState.value.searchWidgetState) {
+            SearchWidgetState.OPENED -> SearchWidgetState.CLOSED
+            SearchWidgetState.CLOSED -> SearchWidgetState.OPENED
+        }
+
+        _uiState.value = _uiState.value.copy(searchWidgetState = newState)
+    }
+
+    fun onFilterTask() {
+        val filterQuery = _uiState.value.filterQuery
+
+        if (filterQuery != TaskStatusFilter.ALL) {
+            val filterResult = _uiState.value.tasksCached.filter {
+                // -1 because TaskStatusFilter enum have more one property
+                it.status.ordinal == filterQuery.ordinal - 1
+            }
+
+            _uiState.value = _uiState.value.copy(tasks = filterResult)
+        } else {
+            val tasksCached = _uiState.value.tasksCached
+            _uiState.value = _uiState.value.copy(tasks = tasksCached)
+        }
+    }
+
+    fun onFilterOptionChange(newValue: TaskStatusFilter) {
+        _uiState.value = _uiState.value.copy(filterQuery = newValue)
+    }
+
+    fun onFilterWidgetStateChange() {
+        val newState = when (_uiState.value.filterWidgetState) {
+            TaskStatusFilterWidgetState.OPENED -> TaskStatusFilterWidgetState.CLOSED
+            TaskStatusFilterWidgetState.CLOSED -> TaskStatusFilterWidgetState.OPENED
+        }
+
+        _uiState.value = _uiState.value.copy(filterWidgetState = newState)
     }
 
     fun onSearchingState(newValue: Boolean) {
         _uiState.value = _uiState.value.copy(isSearching = newValue)
-    }
-
-    fun onTextSearchChange(newValue: String) {
-        _searchTextState.value = newValue
-    }
-
-    fun onSearchWidgetStateChange(newValue: SearchWidgetState) {
-        _searchWidgetState.value = newValue
     }
 }
