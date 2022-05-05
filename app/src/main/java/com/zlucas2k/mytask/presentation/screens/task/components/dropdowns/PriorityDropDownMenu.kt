ad33b387f7c4f8b2f1adec64dffe308056a96a86
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-05-05 02:34:16 -0300

    Adicionado os componentes PriorityDropDownMenu e StatusDropDownMenu

--- app/src/main/java/com/zlucas2k/mytask/presentation/screens/task/components/dropdowns/PriorityDropDownMenu.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/screens/task/components/dropdowns/PriorityDropDownMenu.kt
@@ -1,120 +1,40 @@
 package com.zlucas2k.mytask.presentation.screens.task.components.dropdowns
 
-import android.content.res.Configuration
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+import com.zlucas2k.mytask.R
 import com.zlucas2k.mytask.presentation.common.model.PriorityView
-import com.zlucas2k.mytask.presentation.common.theme.MyTaskTheme
+import com.zlucas2k.mytask.presentation.components.MyTaskDropDownMenu
 
 @Composable
 fun PriorityDropDownMenu(
-    priority: PriorityView,
-    onPrioritySelected: (PriorityView) -> Unit,
-    modifier: Modifier = Modifier
+    onPriorityChange: (PriorityView) -> Unit,
+    menuExpandedState: Boolean,
+    menuExpandedStateChange: () -> Unit,
 ) {
-    val expanded = remember { mutableStateOf(false) }
-    val colorPriority = if (isSystemInDarkTheme()) priority.colorDark else priority.colorLight
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .height(TextFieldDefaults.MinHeight / 2)
-            .clickable { expanded.value = true }
-    ) {
-        Spacer(
-            modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(0.01f)
-                .background(colorPriority)
-        )
-
-        Spacer(modifier = Modifier.size(10.dp))
-
-        Text(
-            text = getPriorityName(priority),
-            style = MaterialTheme.typography.body2,
-            color = MaterialTheme.colors.onBackground.copy(alpha = 0.5f)
-        )
-
-        DropdownMenu(expanded = expanded.value, onDismissRequest = { expanded.value = false }) {
-            DropdownMenuItem(
-                onClick = {
-                    expanded.value = false
-                    onPrioritySelected(PriorityView.NONE)
-                },
-                content = {
-                    PriorityDropDownMenuItem("Sem")
-                }
-            )
-
-            DropdownMenuItem(
-                onClick = {
-                    expanded.value = false
-                    onPrioritySelected(PriorityView.LOW)
-                },
-                content = {
-                    PriorityDropDownMenuItem("Baixa")
-                }
-            )
-
-            DropdownMenuItem(
-                onClick = {
-                    expanded.value = false
-                    onPrioritySelected(PriorityView.MEDIUM)
-                },
-                content = {
-                    PriorityDropDownMenuItem("Média")
-                }
-            )
-
-            DropdownMenuItem(
-                onClick = {
-                    expanded.value = false
-                    onPrioritySelected(PriorityView.HIGH)
-                },
-                content = {
-                    PriorityDropDownMenuItem("Alta")
-                }
-            )
-        }
-    }
-}
-
-@Composable
-private fun PriorityDropDownMenuItem(text: String) {
-    Text(
-        text = text,
-        color = MaterialTheme.colors.onPrimary,
-        style = MaterialTheme.typography.subtitle2
+    val priorityItems = listOf(
+        stringResource(id = R.string.none),
+        stringResource(id = R.string.low),
+        stringResource(id = R.string.medium),
+        stringResource(id = R.string.high)
     )
-}
 
-private fun getPriorityName(priority: PriorityView) = when (priority) {
-    PriorityView.NONE -> "Sem"
-    PriorityView.LOW -> "Baixa"
-    PriorityView.MEDIUM -> "Média"
-    PriorityView.HIGH -> "Alta"
-}
+    val priorityItemsModels = listOf(
+        PriorityView.NONE,
+        PriorityView.LOW,
+        PriorityView.MEDIUM,
+        PriorityView.HIGH,
+    )
 
-@Composable
-@Preview(name = "Light")
-@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
-private fun Preview() {
-    MyTaskTheme {
-        PriorityDropDownMenu(
-            priority = PriorityView.HIGH,
-            onPrioritySelected = {},
-            modifier = Modifier.fillMaxWidth()
-        )
-    }
+    MyTaskDropDownMenu(
+        items = priorityItems,
+        onItemIndexChange = { indexOptionSelected ->
+            onPriorityChange(
+                priorityItemsModels[indexOptionSelected]
+            )
+        },
+        expandedState = menuExpandedState,
+        onExpandedStateChange = menuExpandedStateChange
+    )
 }
--- app/src/main/java/com/zlucas2k/mytask/presentation/screens/task/components/dropdowns/StatusDropDownMenu.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/screens/task/components/dropdowns/StatusDropDownMenu.kt
@@ -1,99 +1,37 @@
 package com.zlucas2k.mytask.presentation.screens.task.components.dropdowns
 
-import android.content.res.Configuration
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.zlucas2k.mytask.R
 import com.zlucas2k.mytask.presentation.common.model.StatusView
-import com.zlucas2k.mytask.presentation.common.theme.MyTaskTheme
+import com.zlucas2k.mytask.presentation.components.MyTaskDropDownMenu
 
 @Composable
 fun StatusDropDownMenu(
-    status: StatusView,
-    onStatusSelected: (StatusView) -> Unit,
-    modifier: Modifier
+    onStatusChange: (StatusView) -> Unit,
+    menuExpandedState: Boolean,
+    menuExpandedStateChange: () -> Unit,
 ) {
-    var expanded by remember { mutableStateOf(false) }
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .height(TextFieldDefaults.MinHeight / 2)
-            .clickable { expanded = true }
-    ) {
-        Icon(
-            imageVector = status.icon,
-            contentDescription = null,
-            tint = MaterialTheme.colors.onPrimary
-        )
-
-        Spacer(modifier = Modifier.size(10.dp))
-
-        StatusTextIndicator(status)
-
-        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-            DropdownMenuItem(
-                onClick = {
-                    expanded = false
-                    onStatusSelected(StatusView.TODO)
-                },
-                content = {
-                    StatusTextIndicator(StatusView.TODO)
-                }
-            )
-
-            DropdownMenuItem(
-                onClick = {
-                    expanded = false
-                    onStatusSelected(StatusView.PROGRESS)
-                },
-                content = {
-                    StatusTextIndicator(StatusView.PROGRESS)
-                }
-            )
-
-            DropdownMenuItem(
-                onClick = {
-                    expanded = false
-                    onStatusSelected(StatusView.DONE)
-                },
-                content = {
-                    StatusTextIndicator(StatusView.DONE)
-                }
-            )
-        }
-    }
-}
-
-@Composable
-private fun StatusTextIndicator(status: StatusView) {
-    val text = when (status) {
-        StatusView.TODO -> "Agendado"
-        StatusView.PROGRESS -> "Em andamento"
-        StatusView.DONE -> "Concluida"
-    }
-
-    Text(
-        text = text,
-        style = MaterialTheme.typography.body2,
-        color = MaterialTheme.colors.onBackground.copy(alpha = 0.5f)
+    val statusItems = listOf(
+        stringResource(id = R.string.todo),
+        stringResource(id = R.string.progress),
+        stringResource(id = R.string.done)
     )
-}
 
-@Composable
-@Preview(name = "Light")
-@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
-private fun Preview() {
-    MyTaskTheme {
-        StatusDropDownMenu(
-            status = StatusView.TODO,
-            onStatusSelected = {},
-            modifier = Modifier.fillMaxWidth()
-        )
-    }
+    val statusItemsModels = listOf(
+        StatusView.TODO,
+        StatusView.PROGRESS,
+        StatusView.DONE,
+    )
+
+    MyTaskDropDownMenu(
+        items = statusItems,
+        onItemIndexChange = { indexOptionSelected ->
+            onStatusChange(
+                statusItemsModels[indexOptionSelected]
+            )
+        },
+        expandedState = menuExpandedState,
+        onExpandedStateChange = menuExpandedStateChange
+    )
 }
