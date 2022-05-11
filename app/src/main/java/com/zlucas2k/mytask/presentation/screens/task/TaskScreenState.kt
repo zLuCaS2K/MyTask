f77c1069d4acc2d1f22e5f02061367b89a045424
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-05-11 11:27:12 -0300

    Remoção da complementa de tudo relacionado a TaskScreen

--- app/src/main/java/com/zlucas2k/mytask/presentation/screens/task/TaskScreenState.kt
+++
@@ -1,8 +0,0 @@
-package com.zlucas2k.mytask.presentation.screens.task
-
-import com.zlucas2k.mytask.presentation.common.model.TaskView
-
-data class TaskScreenState(
-    val task: TaskView = TaskView(),
-    val isEditing: Boolean = false
-)
--- app/src/main/java/com/zlucas2k/mytask/presentation/screens/task/common/TaskEventUI.kt
+++
@@ -1,7 +0,0 @@
-package com.zlucas2k.mytask.presentation.screens.task.common
-
-sealed class TaskEventUI {
-    object SaveTask : TaskEventUI()
-    object DeleteTask : TaskEventUI()
-    data class ShowToast(val message: String) : TaskEventUI()
-}
