1de079ccf54a98a652a1bc5af0724579f96b6446
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-05-16 15:01:08 -0300

    Mudança de var para val da TaskView e uso da função emptyString() para parâmetros padrões

--- app/src/main/java/com/zlucas2k/mytask/presentation/common/model/TaskView.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/common/model/TaskView.kt
@@ -1,11 +1,14 @@
 package com.zlucas2k.mytask.presentation.common.model
 
+import com.zlucas2k.mytask.common.utils.emptyString
+
+// TODO: Mudar o tipo do id para Long
 data class TaskView(
-    var id: Int = 0,
-    var title: String = "",
-    var date: String = "",
-    var time: String = "",
-    var description: String = "",
-    var priority: PriorityView = PriorityView.NONE,
-    var status: StatusView = StatusView.TODO
+    val id: Int = 0,
+    val title: String = emptyString(),
+    val date: String = emptyString(),
+    val time: String = emptyString(),
+    val description: String = emptyString(),
+    val priority: PriorityView = PriorityView.NONE,
+    val status: StatusView = StatusView.TODO
 )
