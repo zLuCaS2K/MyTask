74684868f9b6e457ea3e042136239a052aa710fe
zLuCaS2K <LuucasSantos.Dev@Gmail.com>
2022-05-16 15:01:33 -0300

    Remoção do taskDetailSheet do HomeScreenState

--- app/src/main/java/com/zlucas2k/mytask/presentation/screens/home/HomeScreenState.kt
+++ app/src/main/java/com/zlucas2k/mytask/presentation/screens/home/HomeScreenState.kt
@@ -3,6 +3,5 @@ package com.zlucas2k.mytask.presentation.screens.home
 import com.zlucas2k.mytask.presentation.common.model.TaskView
 
 data class HomeScreenState(
-    val tasks: List<TaskView> = emptyList(),
-    val taskDetailSheet: TaskView = TaskView()
+    val tasks: List<TaskView> = emptyList()
 )
